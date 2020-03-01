6e89c5332fa4b0601fc9b3eeceab604f14ca4d53
everdessoares <isgreen@madeinweb.com.br>
2020-03-01 05:13:51 -0300

    increase min sdk version to 21

--- app/src/main/java/br/com/isgreen/decimaledittext/DecimalEditText.kt
+++ app/src/main/java/br/com/isgreen/decimaledittext/DecimalEditText.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.text.*
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.widget.doAfterTextChanged
 import java.text.DecimalFormat
 import java.text.NumberFormat
 
@@ -19,68 +20,135 @@ class DecimalEditText(context: Context, attrs: AttributeSet) : AppCompatEditText
         const val INT_MAX_VALUE = 15
     }
 
+    private var mValue = 0.0
     private var mPrefix = ""
     private var mSuffix = ""
+    private var mIsUpdating = false
     private var mMaxValue = Float.MAX_VALUE
     private var mMaxIntegerDigit = INT_MAX_VALUE
     private var mMaxDecimalDigit = INT_MAX_VALUE
 
+    var value: Double
+        get() = mValue
+        set(value) {
+            mValue = value
+            setText(value.toString())
+        }
+
+    init {
+        init(context, attrs)
+    }
+
     private fun init(context: Context, attrs: AttributeSet) {
-        val tpArray = context.obtainStyledAttributes(
-            attrs, R.styleable.DecimalEditText
-        )
+        if (!isInEditMode) {
+            val tpArray = context.obtainStyledAttributes(
+                attrs, R.styleable.DecimalEditText
+            )
 
-        val prefix = tpArray.getString(
-            R.styleable.DecimalEditText_prefix
-        )
-        prefix?.let {
-            mPrefix = it
+            val prefix = tpArray.getString(
+                R.styleable.DecimalEditText_prefix
+            )
+            prefix?.let {
+                mPrefix = it
+            }
+
+            val suffix = tpArray.getString(
+                R.styleable.DecimalEditText_suffix
+            )
+            suffix?.let {
+                mSuffix = it
+            }
+
+            mMaxValue = tpArray.getFloat(
+                R.styleable.DecimalEditText_maxValue, Float.MAX_VALUE
+            )
+
+            mMaxIntegerDigit = tpArray.getInt(
+                R.styleable.DecimalEditText_maxIntegerDigit, INT_MAX_VALUE
+            )
+
+            mMaxDecimalDigit = tpArray.getInt(
+                R.styleable.DecimalEditText_maxDecimalDigit, INT_MAX_VALUE
+            )
+
+            this.inputType = InputType.TYPE_CLASS_NUMBER
+            this.doAfterTextChanged { editable ->
+                onAfterTextChanged(editable)
+            }
+
+            var maxLength = mMaxIntegerDigit + mMaxDecimalDigit + 1
+
+            if (mPrefix.isNotEmpty()) {
+                maxLength += mPrefix.length + PREFIX_SPACE_SIZE
+            }
+
+            if (mSuffix.isNotEmpty()) {
+                maxLength += mSuffix.length + SUFFIX_SPACE_SIZE
+            }
+
+            filters = arrayOf(
+                InputFilter.LengthFilter(maxLength)
+            )
+
+            // If has no hint set zero value as default
+            if (hint.isNullOrEmpty()) {
+                value = 0.0
+            }
+
+            tpArray.recycle()
+        }
+    }
+
+    private fun onAfterTextChanged(editable: Editable?) {
+        if (mIsUpdating) {
+            mIsUpdating = false
+            return
         }
 
-        val suffix = tpArray.getString(
-            R.styleable.DecimalEditText_suffix
-        )
-        suffix?.let {
-            mSuffix = it
+        val oldValue = mValue
+        mValue = convertTextToValue(editable?.toString())
+
+        if (mValue == 0.0 && !hint.isNullOrEmpty()) {
+            mIsUpdating = true
+            setText("")
+            return
         }
 
-        mMaxValue = tpArray.getFloat(
-            R.styleable.DecimalEditText_maxValue, Float.MAX_VALUE
-        )
-
-        mMaxIntegerDigit = tpArray.getInt(
-            R.styleable.DecimalEditText_maxIntegerDigit, INT_MAX_VALUE
-        )
-
-        mMaxDecimalDigit = tpArray.getInt(
-            R.styleable.DecimalEditText_maxDecimalDigit, INT_MAX_VALUE
-        )
-
-        this.inputType = InputType.TYPE_CLASS_NUMBER
-        this.addTextChangedListener(onTextChange)
-
-        var maxLength = mMaxIntegerDigit + mMaxDecimalDigit + 1
-
-        if (mPrefix.isNotEmpty()) {
-            maxLength += mPrefix.length + PREFIX_SPACE_SIZE
+        if (mValue > mMaxValue) {
+            mValue = oldValue
         }
+
+        val formatted = formatValue(mValue)
+
+        mIsUpdating = true
+
+        this@DecimalEditText.setText(formatted)
 
         if (mSuffix.isNotEmpty()) {
-            maxLength += mSuffix.length + SUFFIX_SPACE_SIZE
+            this@DecimalEditText.setSelection(formatted.length - SUFFIX_SPACE_SIZE - mSuffix.length)
+        } else {
+            this@DecimalEditText.setSelection(formatted.length)
+        }
+    }
+
+    private fun formatValue(value: Double): String {
+        val fmt = NumberFormat.getInstance() as DecimalFormat
+        fmt.isGroupingUsed = true
+        fmt.positiveSuffix = if (mSuffix.isNotEmpty()) " $mSuffix" else ""
+        fmt.negativeSuffix = if (mSuffix.isNotEmpty()) " $mSuffix" else ""
+        fmt.positivePrefix = if (mPrefix.isNotEmpty()) "$mPrefix " else ""
+        fmt.negativePrefix = if (mPrefix.isNotEmpty()) "$mPrefix -" else ""
+        fmt.minimumFractionDigits = mMaxDecimalDigit
+        fmt.maximumFractionDigits = mMaxDecimalDigit
+        fmt.maximumIntegerDigits = mMaxIntegerDigit
+        return fmt.format(value)
+    }
+
+    private fun convertTextToValue(text: String?): Double {
+        if (text.isNullOrEmpty()) {
+            return 0.0
         }
 
-        filters = arrayOf(
-            InputFilter.LengthFilter(maxLength)
-        )
-
-        tpArray.recycle()
-    }
-
-    fun getValue(): Double {
-        return if (this.text.isNullOrBlank()) 0.0 else getValue(this.text.toString())
-    }
-
-    fun getValue(text: String): Double {
         val symbols = "[$mPrefix$mSuffix,.€]"
 
         val cleanString = text.replace(symbols.toRegex(), "")
@@ -97,80 +165,22 @@ class DecimalEditText(context: Context, attrs: AttributeSet) : AppCompatEditText
     }
 
     override fun onSelectionChanged(start: Int, end: Int) {
-        val text = text
-        if (text != null) {
-            if (start != text.length || end != text.length) {
-                if (mSuffix.isNotEmpty()) {
-                    Selection.setSelection(text, text.length - SUFFIX_SPACE_SIZE - mSuffix.length)
-                } else {
-                    setSelection(text.length, text.length)
+        if (isLaidOut) {
+            val text = text
+            if (text != null) {
+                var position = if (mSuffix.isEmpty()) text.length
+                else text.length - SUFFIX_SPACE_SIZE - mSuffix.length
+
+                if (position < 0) {
+                    position = 0
                 }
-                return
+
+                if (start != position || end != position) {
+                    Selection.setSelection(text, position)
+                    return
+                }
             }
         }
-
-        super.onSelectionChanged(start, end)
-    }
-
-    private fun valueFormatter(value: Double): String {
-        val fmt = NumberFormat.getInstance() as DecimalFormat
-        fmt.isGroupingUsed = true
-        fmt.positiveSuffix = if (mSuffix.isNotEmpty()) " $mSuffix" else ""
-        fmt.negativeSuffix = if (mSuffix.isNotEmpty()) " $mSuffix" else ""
-        fmt.positivePrefix = if (mPrefix.isNotEmpty()) "$mPrefix " else ""
-        fmt.negativePrefix = if (mPrefix.isNotEmpty()) "$mPrefix -" else ""
-        fmt.minimumFractionDigits = mMaxDecimalDigit
-        fmt.maximumFractionDigits = mMaxDecimalDigit
-        fmt.maximumIntegerDigits = mMaxIntegerDigit
-        return fmt.format(value)
-    }
-
-    private val onTextChange = object : TextWatcher {
-
-        private var oldValue: Double = 0.0
-        private var isUpdating: Boolean = false
-
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
-            if (!TextUtils.isEmpty(s)) {
-                oldValue = getValue(s.toString())
-            }
-        }
-
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-
-        }
-
-        override fun afterTextChanged(s: Editable) {
-            if (isUpdating) {
-                isUpdating = false
-                return
-            }
-
-            val text =
-                if (TextUtils.isEmpty(s.toString())) valueFormatter(0.0) else s.toString()
-
-            var value = getValue(text)
-
-            if (value > mMaxValue) {
-                value = oldValue
-            }
-
-            val formatted = valueFormatter(value)
-
-            isUpdating = true
-
-            this@DecimalEditText.setText(formatted)
-
-            if (mSuffix.isNotEmpty()) {
-                this@DecimalEditText.setSelection(formatted.length - SUFFIX_SPACE_SIZE - mSuffix.length)
-            } else {
-                this@DecimalEditText.setSelection(formatted.length)
-            }
-        }
-    }
-
-    init {
-        init(context, attrs)
     }
 
 }
